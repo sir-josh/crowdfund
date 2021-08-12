daef8ecefa9cbb38861d6c6d2998eeedf2483f6a
Aniekwe Joshua <joshuaaniekwe@gmail.com>
2021-08-12 22:10:01 +0100

    redeploy to get new network address

--- ethereum/factory.js
+++ ethereum/factory.js
@@ -3,7 +3,7 @@ import crowdfundFactory from './build/CrowdFundFactory.json';
 
 const CrowdFundFactoryInstance = new web3.eth.Contract(
     JSON.parse(crowdfundFactory.interface), 
-    "0xab6DE12c4a8621596cc3b9D83c8b8f125e2eC483"
+    "0x7263CC0f4502376Fb08A31587338924BEb417B10"
 );
 
 export default CrowdFundFactoryInstance;
