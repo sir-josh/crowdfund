eb2f936bc046da5e4522f454e164c09d48adbbce
Aniekwe Joshua <joshuaaniekwe@gmail.com>
2021-08-14 05:05:47 +0100

    style up the homepage a little

--- components/Layout.js
+++ components/Layout.js
@@ -1,10 +1,14 @@
-import React from 'react';
+import React, { Fragment } from 'react';
+import styles from './Layout.module.css';
+// import './styles.css';
 
 export default (props) => {
     return (
-        <div>
-            <h1>I am the header</h1>
-            {props.children}
+        <div className={styles.body}>
+            <h1 className={styles.layoutHeader}>CROWDFUND</h1>
+            <div className={styles.layoutBody}>
+                {props.children}
+            </div>
             <h1>I am a footer</h1>
         </div>
     )
