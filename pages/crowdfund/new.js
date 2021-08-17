7535f804c19097ff4a861de2e7c2acf22419f2d7
Aniekwe Joshua <joshuaaniekwe@gmail.com>
2021-08-17 21:40:59 +0100

    form error handling

--- pages/crowdfund/new.js
+++ pages/crowdfund/new.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
-import { Form, Button,Input } from 'semantic-ui-react';
+import { Form, Button, Input, Message } from 'semantic-ui-react';
 import Layout from '../../components/Layout';
 import factory from '../../ethereum/factory';
 import web3 from '../../ethereum/web3';
 
 export class NewCrowdfund extends Component {
     state = {
-        minimumContribution: ''
+        minimumContribution: '',
+        errorMessage: ''
     }
 
     submitFormCreation = async(event) => {
@@ -14,18 +15,25 @@ export class NewCrowdfund extends Component {
 
         const accounts = await web3.eth.getAccounts();
 
-        await factory.methods.deployNewCrowdFund(
-            this.state.minimumContribution
-        ).send({
-            from: accounts[0]
-        });
+        try {
+            await factory.methods.deployNewCrowdFund(
+                this.state.minimumContribution, 
+                "Project Name",
+                "Project Description",
+                "Project Owner"
+            ).send({
+                from: accounts[0]
+            });
+        } catch (error) {
+            this.setState({ errorMessage: error.message });
+        }
     }
 
     render() {
         return (
             <Layout>
                 <h3>Create New Crowdfund project</h3>
-                <Form onSubmit={this.submitFormCreation}>
+                <Form onSubmit={this.submitFormCreation} error={!!this.state.errorMessage}>
                     <Form.Field>
                         <label>Minimum contribution (wei)</label>
                         <Input 
@@ -39,6 +47,7 @@ export class NewCrowdfund extends Component {
                             }
                         />
                     </Form.Field>
+                    <Message error header="Oops!" content={this.state.errorMessage} />
                     <Button content="Create project" style={{ backgroundColor: '#282c34',color: "white" }}></Button>
                 </Form>
             </Layout>
