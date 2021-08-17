73e84cc767e65170455b33508269ceab720d38f4
Aniekwe Joshua <joshuaaniekwe@gmail.com>
2021-08-17 19:35:36 +0100

    create form submittal and test submittal

--- pages/crowdfund/new.js
+++ pages/crowdfund/new.js
@@ -1,17 +1,31 @@
 import React, { Component } from 'react';
 import { Form, Button,Input } from 'semantic-ui-react';
 import Layout from '../../components/Layout';
+import factory from '../../ethereum/factory';
+import web3 from '../../ethereum/web3';
 
 export class NewCrowdfund extends Component {
     state = {
         minimumContribution: ''
     }
 
+    submitFormCreation = async(event) => {
+        event.preventDefault();
+
+        const accounts = await web3.eth.getAccounts();
+
+        await factory.methods.deployNewCrowdFund(
+            this.state.minimumContribution
+        ).send({
+            from: accounts[0]
+        });
+    }
+
     render() {
         return (
             <Layout>
                 <h3>Create New Crowdfund project</h3>
-                <Form>
+                <Form onSubmit={this.submitFormCreation}>
                     <Form.Field>
                         <label>Minimum contribution (wei)</label>
                         <Input 
