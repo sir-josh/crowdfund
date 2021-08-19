4b1da11748f437aca5549c17a5a4583be0ee4ff9
Aniekwe Joshua <joshuaaniekwe@gmail.com>
2021-08-19 09:31:29 +0100

    making access to crowdfund methods on the blockchain

--- pages/crowdfund/show.js
+++ pages/crowdfund/show.js
@@ -1,7 +1,18 @@
 import React, { Component } from 'react';
 import Layout from '../../components/Layout';
+import CrowdfundInstance from '../../ethereum/crowdfund';
 
 export class ShowCrowdfund extends Component {
+    static async getInitialProps(props) {
+        const crowdfund = CrowdfundInstance(props.query.address);  //Add in url address paramater to the function
+
+        const crowdfundStats = await crowdfund.methods.getCrowdFundStats().call();
+
+        console.log(crowdfundStats);
+
+        return {};
+    }
+
     render() {
         return (
             <Layout>
