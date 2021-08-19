6a31842d4b2ac66d726d944f43aed1647dbc4408
Aniekwe Joshua <joshuaaniekwe@gmail.com>
2021-08-19 05:02:55 +0100

    add new page and route mapping

---
+++ pages/crowdfund/show.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+export class ShowCrowdfund extends Component {
+    render() {
+        return (
+            <div>
+                <h3>Showing Crowdfund</h3>
+            </div>
+        )
+    }
+}
+
+export default ShowCrowdfund;
--- routes.js
+++ routes.js
@@ -1,3 +1,7 @@
 const routes = require('next-routes')();
 
+routes
+    .add('/crowdfund/new', '/crowdfund/new')
+    .add('/crowdfund/:address', '/crowdfund/show');
+
 module.exports = routes;
